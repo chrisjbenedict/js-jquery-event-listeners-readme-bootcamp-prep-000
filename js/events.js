77502effb5f9489d6b968012beddab9646f3be9f
Christopher Benedict <chrisjbenedict@gmail.com>
2018-09-27 15:06:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,11 +3,19 @@
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  
+  
 });
 
 function getIt() {
-  $('p').on("click", function() {
+  $('p').on('click', function() {
     alert("Hey!")
-  })
+  });
+}
+
+function framIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  });
 }
